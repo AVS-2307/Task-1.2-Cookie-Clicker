2a585f778b0e196a91d499e2869c6909c3e0fbbe
AVS-2307 <andrey230781.an@gmail.com>
2023-02-16 14:53:32 +0300

    ternary operator for img

--- task.js
+++ task.js
@@ -1,5 +1,5 @@
 
-function clickCookie() {
+/* function clickCookie() {
     let clickAmount = 0;
     const img = document.querySelector('img');
 
@@ -22,6 +22,23 @@ function clickCookie() {
         document.getElementById('clicker__timer').innerText = speedClick;
         lastClick = currentTime;
     };
+} */
+
+
+function clickCookie() {
+    let clickAmount = 0;
+    let clickToDisplay = document.getElementById('clicker__counter');
+    let img = document.querySelector('img');
+    let lastClick = 0; 
+    document.getElementById('cookie').onclick = function ClickCounter() {               
+        clickAmount++;
+        clickToDisplay.textContent = clickAmount; 
+        img.width = clickToDisplay.textContent % 2 ? 250 : 200;
+        let currentTime = new Date();
+        speedClick =  (1 / ((currentTime - lastClick) / 1000)).toFixed(2);
+        document.getElementById('clicker__timer').innerText = speedClick;
+        lastClick = currentTime;
+    }
 }
 
 
